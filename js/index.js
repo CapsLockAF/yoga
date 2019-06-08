211ef9072de5b45d63e518c4716a81f74465bf8c
CapsLockAF <antiffanarr@gmail.com>
2019-06-08 11:31:42 +0300

    added show-hide tabcontent

--- js/index.js
+++ js/index.js
@@ -1,6 +1,18 @@
 window.addEventListener('DOMContentLoaded', function(){
     'use strict';
 
-    
+    const wraptabs = document.querySelector('.info-header'),
+          tabs = document.querySelectorAll('.info-header-tab'),
+          tabContent =document.querySelectorAll('.info-tabcontent');
 
+    function hideTabContent(a) {
+        for (let i = a; i < tabContent.length; i++) {
+            tabContent[a].classList.remove('show');
+            tabContent[a].classList.add('hide');
+        }
+    }
+
+    hideTabContent(1);
+
+    
 });
