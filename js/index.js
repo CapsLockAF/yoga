5a7e5d8a6c28379d3bc99cfc51b6d7ab3db4fa74
CapsLockAF <antiffanarr@gmail.com>
2019-06-08 16:01:53 +0300

    addet timer index.js

--- js/index.js
+++ js/index.js
@@ -34,4 +34,52 @@ window.addEventListener('DOMContentLoaded', function(){
             }
         }
     });
+
+    // TIMER
+
+    let deadline = '2019-12-21';
+
+    function getTimeRemaining(endtime) {
+        let t = Date.parse(endtime) - Date.parse(new Date()),
+            seconds = Math.floor((t/1000) % 60),
+            minutes = Math.floor((t/1000/60) % 60),
+            hours = Math.floor((t/(1000*60*60)));
+            // hours = Math.floor((t/1000/60/60) % 24)
+            // days = Math.floor((t/(1000*60*60*24)))
+        return {
+            'total': t,
+            'hours': hours,
+            'minutes': minutes,
+            'seconds': seconds
+        };
+    }
+
+    function setClock(id, endtime) {
+        const timer = document.querySelector(id),
+              hours = timer.querySelector('.hours'),
+              minutes = timer.querySelector('.minutes'),
+              seconds = timer.querySelector('.seconds'),
+              timeInterval = setInterval(updateClock, 1000);
+        
+        function padStart(time){
+            if(time < 10){
+                time = '0' + time;
+            }
+            return time;
+        }
+
+        function updateClock() {
+            let t = getTimeRemaining(endtime);
+           
+            hours.textContent = padStart(t.hours);
+            minutes.textContent = padStart(t.minutes);
+            seconds.textContent = padStart(t.seconds);
+            
+            if (t.total <= 0){
+                clearInterval(timeInterval);
+            }
+        }
+    }
+
+    setClock('#timer', deadline);
 });
